33290e3f46fc1cda2cea4d334b8851e34b8b5785
Matthew Sah <mattsah2001@gmail.com>
2024-01-20 21:55:39 -0800

    completed 4.1

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/matthewsah/swe262p_ex_2.git
+
+go 1.20
--- src/term_frequency_4_1.go
+++ src/term_frequency_4_1.go
@@ -14,18 +14,20 @@ type pair struct {
 }
 
 func main() {
-	// word_freqs := make([]pair, 0)
+	wordFreqs := make([]pair, 0)
 
-	stopWords, err := os.ReadFile("../stop_words.txt")
+	stopwords, err := os.ReadFile("../stop_words.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	stopWordsList := strings.Split(string(stopWords), ",")
-	asciiVal := 65
+	stopwordsList := strings.Split(string(stopwords), ",")
+	asciiVal := 97
 	for i := 0; i < 26; i++ {
 		char := string(rune(asciiVal + i))
-		stopWordsList = append(stopWordsList, char)
+		stopwordsList = append(stopwordsList, char)
 	}
+
+	fmt.Println(stopwordsList)
 
 	file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -34,9 +36,61 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	startChar := -1
+
 	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+		line := scanner.Text() + " "
+		startChar = -1
+
+		for idx, char := range line {
+			if startChar == -1 {
+				if rune('a') <= char && char <= rune('z') || rune('A') <= char && char <= rune('Z') || rune('0') <= char && char <= rune('9') {
+					startChar = idx
+				}
+			} else {
+				if !(rune('a') <= char && char <= rune('z') || rune('A') <= char && char <= rune('Z') || rune('0') <= char && char <= rune('9')) {
+					found := false
+					word := strings.ToLower(line[startChar:idx])
+
+					isStopword := false
+					for _, stopword := range stopwordsList {
+						if word == stopword {
+							isStopword = true
+							break
+						}
+					}
+
+					if !isStopword {
+						pairIdx := 0
+						for idx, curPair := range wordFreqs {
+							if word == curPair.word {
+								wordFreqs[idx] = pair{curPair.word, curPair.count + 1}
+								found = true
+								break
+							}
+							pairIdx += 1
+						}
+
+						if !found {
+							wordFreqs = append(wordFreqs, pair{word, 1})
+						} else {
+							for n := pairIdx - 1; n >= 0; n-- {
+								if wordFreqs[pairIdx].count > wordFreqs[n].count {
+									wordFreqs[n], wordFreqs[pairIdx] = wordFreqs[pairIdx], wordFreqs[n]
+									pairIdx--
+								} else {
+									break
+								}
+							}
+						}
+					}
+					startChar = -1
+				}
+			}
+		}
 	}
 
+	for _, pair := range wordFreqs[:25] {
+		fmt.Println(pair.word, "-", pair.count)
+	}
 }
